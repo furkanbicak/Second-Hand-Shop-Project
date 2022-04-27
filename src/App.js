f0734f3329df3c0aeae3816c37b84e600f002d8a
afurkanbicak <afurkanbicak@gmail.com>
2022-04-27 16:19:23 +0300

    router structure build
    Router yapısı oluşturuldu ve yönlendirmeler gerçekleştirildi.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Router from './rooter/rooter';
 
 function App() {
   return (
     <div className="App">
-     
+      <Router />
     </div>
   );
 }
---
+++ src/rooter/rooter.js
@@ -0,0 +1,37 @@
+import Register from '../pages/register/register';
+import Login from '../pages/login/login';
+import Home from '../pages/home/home';
+import ProductDetail from '../pages/productDetail/productDetail'
+import MyAccount from '../pages/myAccount/myAccount'
+import ProductAdd from '../pages/productAdd/productAdd'
+import Missing from '../pages/missing/missing'
+import Layout from '../components/layout';
+import { Route, Routes } from 'react-router-dom'
+
+function Router() {
+  return (
+    <Routes>
+    <Route path="/" element={<Layout />}>
+
+      {/* public routes */}
+      <Route path="home" element={<Home />} />
+      <Route path="login" element={<Login />} />
+      <Route path="register" element={<Register />} />
+      
+        {/* we want to protect these routes */}
+      
+        <Route path="productDetail" element={<ProductDetail />} />
+      
+        <Route path="myAccount" element={<MyAccount />} />
+      
+        <Route path="productAdd" element={<ProductAdd />} />
+
+
+        {/* catch all */}
+        <Route path="*" element={<Missing />} />
+    </Route>
+  </Routes>
+  )
+}
+
+export default Router;
