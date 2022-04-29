f0913424cf158a54c1e95594caa6ed718d928e7f
afurkanbicak <afurkanbicak@gmail.com>
2022-04-29 17:52:42 +0300

    axios contains regulations

--- src/api/axios.js
+++ src/api/axios.js
@@ -4,10 +4,10 @@ export const baseURL = 'https://bootcamp.akbolat.net/';
 export default axios.create({ baseURL });
 
 export const URL = {
-    register: '/auth/local/register',
-    login: '/auth/local',
-    brands: '/brands',
-    categories: '/categories',
-    colors:'/colors',
-    products:'/products'
+    register    :   '/auth/local/register',
+    login       :   '/auth/local',
+    brands      :   '/brands',
+    categories  :   '/categories',
+    colors      :   '/colors',
+    products    :   '/products'
 }
