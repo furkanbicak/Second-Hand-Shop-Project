77e26c425ecb8fd9b1a6766da3e234cbf174db79
afurkanbicak <afurkanbicak@gmail.com>
2022-04-29 10:42:57 +0300

    main component built for service

---
+++ src/api/axios.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+export const baseURL = 'https://bootcamp.akbolat.net/';
+export default axios.create({ baseURL });
+
+export const URL = {
+    register: '/auth/local/register',
+    login: '/auth/local',
+    brands: '/brands',
+    categories: '/categories',
+    colors:'/colors',
+    products:'/products'
+}
