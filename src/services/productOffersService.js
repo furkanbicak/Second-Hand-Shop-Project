aa8087371ec63054e78de38c3dd3507b6821f82f
afurkanbicak <afurkanbicak@gmail.com>
2022-05-04 09:47:48 +0300

    Service to quote the product price

---
+++ src/services/productOffersService.js
@@ -0,0 +1,35 @@
+import axios, { URL } from "../api/axios";
+
+const token = localStorage.getItem('Token');
+
+const config = {
+    headers: { Authorization: `Bearer ${token}` }
+};
+
+// teklif vermek için servis.
+export const postOffers = async (id, userId, offerPrice) => {
+
+    try {
+        const res = await axios.post(`${URL.offers}`,
+            {
+                "product": id,
+                "users_permissions_user": userId,
+                "offerPrice": offerPrice
+            }, 
+            config,
+        );
+        
+        if(res.status === 200){
+            console.log("Ürünler İD", res.data)
+            return res.data
+        } 
+        else{
+            return {
+                error: 'Data gelmedi'
+            }
+        }
+       
+    } catch (error) {
+        console.log("error")
+    }
+}
